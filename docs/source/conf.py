82ecd20deaa6ba4a6dee22b823da62d9c11f55e0
John Wooten <john.wooten@blocktransfer.com>
2024-05-30 12:18:18 -0500

    📄 Streamline documentation word choices

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,13 +4,16 @@ from recommonmark.transform import AutoStructify
 
 sys.path.insert(0, os.path.abspath('.'))
 
+# Project information
 project = 'Web3 Transfer Agent Depository'
 copyright = '2024, BlockTrans Syndicate'
-author = 'BlockTrans Syndicate'
+author = 'Block Transfer'
 
+# Release and version
 release = '0.1'
 version = '0.1.0'
 
+# Extensions
 extensions = [
     'myst_parser',
     'sphinx.ext.duration',
@@ -21,29 +24,23 @@ extensions = [
     'sphinx_markdown_tables',
 ]
 
+# Intersphinx mapping
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
+# Source suffixes
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 
+# Templates path
 templates_path = ['_templates']
 
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-master_doc = 'index'
-html_static_path = ['_static']
-html_favicon = '_static/favicon.ico'
-
+# HTML output options
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'collapse_navigation': False,
@@ -54,16 +51,22 @@ html_theme_options = {
     'link': '#AC51FF',
     'link_hover': '#682D8B',
 }
+html_static_path = ['_static']
+html_favicon = '_static/favicon.ico'
 
+# EPUB output options
+epub_show_urls = 'footnote'
+
+# Master document
+master_doc = 'index'
+
+# Custom setup function
 def setup(app):
     app.add_transform(AutoStructify)
-    
-# CSS not working RN
-# def setup(app):
-#     app.add_css_file('custom.css')
-#
-# html_context = {
-#     'css_files': [
-#         'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
-#     ],
-# }
+    # If you need custom CSS, uncomment and modify the lines below
+    # app.add_css_file('custom.css')
+    # html_context = {
+    #     'css_files': [
+    #         'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
+    #     ],
+    # }
