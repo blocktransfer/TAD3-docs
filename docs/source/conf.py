8fe87fdf075d20e6a735b82f46cd1e2193cc7412
John Wooten <john.wooten@blocktransfer.com>
2024-05-30 12:11:22 -0500

    🔧 Fix ToC duplication issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,10 +56,6 @@ html_theme_options = {
 }
 
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'url_resolver': lambda url: github_doc_root + url,
-        'auto_toc_tree_section': 'Contents',
-    }, True)
     app.add_transform(AutoStructify)
     
 # CSS not working RN
